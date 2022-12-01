b63dbb8d5b91237405837e44237451ee3a39e60c
Vadim <vadim_kimm@icloud.com>
2022-12-01 13:21:34 +0300

    Remove viewModel from LR1

--- ReliabilityInASDU/Sources/Scenes/LROne/View/ContentViewLROne.swift
+++ ReliabilityInASDU/Sources/Scenes/LROne/View/ContentViewLROne.swift
@@ -8,16 +8,14 @@
 import SwiftUI
 
 struct ContentViewLROne: View {
-    
-    //    var viewModel: ContentViewModelLROne?
     @Binding var selectedView: Int
-    
+
     @ObservedObject var data = ParseData()
     @State var result = ""
     @State var Pk: String = String(IntensityMistakes.Probabilities.Pk)
     @State var Pob: String = String(IntensityMistakes.Probabilities.Pob)
     @State var Pi: String = String(IntensityMistakes.Probabilities.Pi)
-    
+
     var body: some View {
         HStack {
             VStack(alignment: .leading) {
@@ -25,30 +23,30 @@ struct ContentViewLROne: View {
                               title: "Add",
                               action: { addButtonPressed() })
                 .padding(.leading, 20)
-                
+
                 TabButtonView(image: "square.and.arrow.down",
                               title: "Save",
                               action: { saveButtonPressed() })
                 .padding(.leading, 20)
-                
+
                 TabButtonView(image: "equal",
                               title: "Compute",
                               action: { computeButtonPressed() })
                 .padding(.leading, 20)
-                
+
                 TextField("", text: $result)
                     .frame(width: 70)
                     .padding(.leading, 20)
-                
+
                 Spacer()
             }
             .frame(width: 80)
             .padding(.top, 40)
-            
+
             VStack {
                 HeaderView()
                     .padding(.top, 10)
-                
+
                 List {
                     ForEach($data.intensityMistakes) { $item in
                         VStack {
@@ -57,27 +55,27 @@ struct ContentViewLROne: View {
                                     TextEditor(text: $item.description)
                                         .background(.bar)
                                         .menuIndicator(.hidden)
-                                    
+
                                     Text(item.description)
                                         .opacity(0)
                                         .padding(.all, 8)
                                         .font(.callout)
                                 }
                                 .frame(minWidth: 150)
-                                
+
                                 TextField("", text: $item.intensityMistakes)
                                     .padding(.leading, 30)
                                     .foregroundColor(item.intensityMistakes.isNumeric ? .white : .red)
-                                
+
                                 TextField("", text: $item.numberOfOperations)
                                     .padding(.leading, 30)
                                     .foregroundColor(item.numberOfOperations.isNumeric ? .white : .red)
-                                
+
                                 TextField("", text: $item.averageTime)
                                     .padding(.leading, 30)
                                     .foregroundColor(item.averageTime.isNumeric ? .white : .red)
                             }
-                            
+
                             Divider()
                         }
                     }
@@ -87,7 +85,7 @@ struct ContentViewLROne: View {
                 }
             }
             .frame(width: 750)
-            
+
             VStack(alignment: .leading) {
                 HStack {
                     Text("Pk:   ")
@@ -95,21 +93,21 @@ struct ContentViewLROne: View {
                         .frame(width: 50)
                         .foregroundColor(Pk.isNumeric ? .white : .red)
                 }
-                
+
                 HStack {
                     Text("Pob: ")
                     TextField("", text: $Pob)
                         .frame(width: 50)
                         .foregroundColor(Pob.isNumeric ? .white : .red)
                 }
-                
+
                 HStack {
                     Text("Pi:    ")
                     TextField("", text: $Pi)
                         .frame(width: 50)
                         .foregroundColor(Pi.isNumeric ? .white : .red)
                 }
-                
+
                 Spacer()
             }
             .frame(width: 150)
@@ -138,15 +136,15 @@ struct ContentView_Previews: PreviewProvider {
 // MARK: - Actions
 
 extension ContentViewLROne {
-    
+
     private func addButtonPressed() {
         data.loadBlankModel()
     }
-    
+
     private func saveButtonPressed() {
         data.saveData()
     }
-    
+
     private func submitButtonPressed() {
         setNewConstants(Pk: Double(Pk),
                         Pob: Double(Pob),
@@ -162,24 +160,24 @@ extension ContentViewLROne {
         let result = computeProbability(array: data.intensityMistakes)
         self.result = result
     }
-    
+
     private func computeProbability(array:  [IntensityMistakes]) -> String {
         var Pop = Double()
         var expPower = Double()
         let Pisp = IntensityMistakes.Probabilities.Pk * IntensityMistakes.Probabilities.Pob * IntensityMistakes.Probabilities.Pi
-        
+
         for item in array {
             let baseAndPower = item.intensityMistakes.convertToDemical()
             let base = baseAndPower.0
             let power = baseAndPower.1
-            
+
             expPower += (base * pow(10, -power)) * (Double(item.averageTime) ?? 1) * (Double(item.numberOfOperations) ?? 1)
         }
-        
+
         Pop = exp(-expPower)
         return String(format: "%.4f", (Pop + (1 - Pop) * Pisp))
     }
-    
+
     private func setNewConstants(Pk: Double?, Pob: Double?, Pi: Double?) {
         IntensityMistakes.Probabilities.Pk = Pk ?? IntensityMistakes.Probabilities.Pk
         IntensityMistakes.Probabilities.Pob = Pob ?? IntensityMistakes.Probabilities.Pob

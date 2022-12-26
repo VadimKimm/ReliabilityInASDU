85f3c7848a133861583285118ac3ca2490abcc9e
Vadim <vadim_kimm@icloud.com>
2022-12-26 14:14:19 +0300

    create JMTableView

--- ReliabilityInASDU/Sources/Scenes/LRFour/View/JMTableView.swift
+++ ReliabilityInASDU/Sources/Scenes/LRFour/View/JMTableView.swift
@@ -6,15 +6,40 @@
 //
 
 import SwiftUI
+import OrderedCollections
 
 struct JMTableView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+    var dataForTable: OrderedDictionary<Double, Double>?
 
-struct JMTableView_Previews: PreviewProvider {
-    static var previews: some View {
-        JMTableView()
+    var body: some View {
+        VStack {
+            Text("Таблица значений")
+                .font(.title)
+                .padding(.bottom, 10)
+
+            VStack {
+                HStack {
+                    Text("Время")
+                    Spacer()
+                    Text("Вероятность того, что в программе нет ошибок")
+                        .fixedSize(horizontal: false, vertical: true)
+                }
+                .padding(.all, 5)
+                .font(.title3)
+                .frame(width: 200)
+
+                List(1..<((dataForTable?.count ?? 0)) + 1) { xValue in
+                    HStack {
+                        Text(String(xValue))
+                        Spacer()
+                        Text(String(format: "%.3f", dataForTable?.elements[xValue - 1].value ?? 0.0))
+                    }
+                    .padding(.all, 5)
+                }
+                .frame(width: 200)
+                .border(.white)
+            }
+            .border(.white)
+        }
     }
 }
